b7d67c8dd00dbd24bbfa2df948e60098b188aa75
louisehbrt <louise.hubert@octo.com>
2022-06-20 11:14:44 +0200

    Initial commit

---
+++ src/feature_encoding.py
@@ -0,0 +1,99 @@
+from src import config
+import numpy as np
+import pandas as pd
+import joblib
+import category_encoders as ce
+from sklearn.preprocessing import LabelEncoder
+from dirty_cat import SimilarityEncoder, MinHashEncoder, GapEncoder
+
+
+def chose_feature_encoding(df, feature, name_encoder):
+    if name_encoder == 'label':
+        encoder = LabelEncoder()
+        df[feature] = encoder.fit_transform(df[feature])
+
+    elif name_encoder == 'basen':
+        encoder = ce.BaseNEncoder(cols=[feature], return_df=True, base=5)
+        df = encoder.fit_transform(df)
+
+    elif name_encoder == 'similarity':
+        encoder = SimilarityEncoder(similarity='ngram')
+        df[feature] = encoder.fit_transform(np.array(df[feature]).reshape(-1, 1))
+
+    elif name_encoder == 'minhash':
+        encoder = MinHashEncoder(n_components=100)
+        df[feature] = encoder.fit_transform(np.array(df[feature]).reshape(-1, 1))
+
+    elif name_encoder == 'gap':
+        encoder = GapEncoder(n_components=100)
+        df[feature] = encoder.fit_transform(np.array(df[feature]).reshape(-1, 1))
+
+    return df
+
+
+def chose_encoder(df, name, mode):
+    # BaseNEncoder
+    if name == 'basen':
+        encoder = ce.BaseNEncoder(
+            cols=['Course Name', 'Course Skill', 'Specialization', 'Training Provider', 'Main Domain', 'Course Code'],
+            return_df=True, base=5)
+        df = encoder.fit_transform(df)
+
+    # SimilarityEncoder
+    elif name == 'similarity':
+        encoders = {}
+        for c in ['Course Name', 'Course Skill', 'Specialization', 'Training Provider', 'Main Domain', 'Course Code']:
+            encoders[c] = SimilarityEncoder(similarity='ngram')
+            if mode == 'train':
+                df[c] = encoders[c].fit_transform(np.array(df[c]).reshape(-1, 1))
+            else:
+                encoders = joblib.load(config.MODELS_PATH + 'feature_encoders.pkl')
+                df[c] = encoders[c].transform(df[c])
+
+    # MinHashEncoder
+    elif name == 'minhash':
+        encoders = {}
+        for c in ['Course Name', 'Course Skill', 'Specialization', 'Training Provider', 'Main Domain', 'Course Code']:
+            encoders[c] = MinHashEncoder(n_components=100)
+            if mode == 'train':
+                df[c] = encoders[c].fit_transform(np.array(df[c]).reshape(-1, 1))
+            else:
+                encoders = joblib.load(config.MODELS_PATH + 'feature_encoders.pkl')
+                df[c] = encoders[c].transform(df[c])
+
+    # GapEncoder
+    elif name == 'gap':
+        encoders = {}
+        for c in ['Course Name', 'Course Skill', 'Specialization', 'Training Provider', 'Main Domain', 'Course Code']:
+            encoders[c] = GapEncoder(n_components=100)
+            if mode == 'train':
+                df[c] = encoders[c].fit_transform(np.array(df[c]).reshape(-1, 1))
+            else:
+                encoders = joblib.load(config.MODELS_PATH + 'feature_encoders.pkl')
+                df[c] = encoders[c].transform(df[c])
+
+    return df
+
+
+def encode(df):
+    df = chose_feature_encoding(df, 'Course Type', 'label')
+    df = chose_feature_encoding(df, 'Course Status', 'label')
+    df = chose_feature_encoding(df, 'Country/Territory', 'label')
+    df = chose_feature_encoding(df, 'Priority', 'label')
+    df = chose_feature_encoding(df, 'Managed Type', 'label')
+    df = chose_feature_encoding(df, 'Delivery Tool Platform', 'label')
+    df = chose_feature_encoding(df, 'Display Course Type', 'label')
+    df = chose_feature_encoding(df, 'Course Name', 'basen')
+    df = chose_feature_encoding(df, 'Course Skill', 'basen')
+    df = chose_feature_encoding(df, 'Specialization', 'basen')
+    df = chose_feature_encoding(df, 'Training Provider', 'basen')
+    df = chose_feature_encoding(df, 'Main Domain', 'basen')
+    df = chose_feature_encoding(df, 'Course Code', 'basen')
+
+    test = df[df['Year'] == 2021]
+    train = df[df['Year'] < 2021]
+
+    test.to_csv('/Users/louise.hubert/PycharmProjects/training_predictions/data/test_data.csv', index=False)
+    train.to_csv('/Users/louise.hubert/PycharmProjects/training_predictions/data/train_data.csv', index=False)
+
+    return df, train, test
--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 from src import config
+from src import feature_processing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
@@ -20,6 +21,35 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 import pandas as pd
 
+
+def encode(df):
+    columns = ['Course Code', 'Course Name', 'Course Type', 'Course Status', 'Country/Territory', 'Course Skill',
+               'Main Domain', 'Priority', 'Training Provider', 'Managed Type', 'Delivery Tool Platform',
+               'Display Course Type', 'Specialization']
+    n = len(columns)
+    for i in range(n):
+        print(columns[i])
+        df = feature_processing.chose_feature_encoding(df, columns[i],'label')
+    #df = feature_processing.chose_feature_encoding(df, 'Course Status', 'label')
+    #df = feature_processing.chose_feature_encoding(df, 'Country/Territory', 'label')
+    #df = feature_processing.chose_feature_encoding(df, 'Priority', 'label')
+    #df = feature_processing.chose_feature_encoding(df, 'Managed Type', 'label')
+    #df = feature_processing.chose_feature_encoding(df, 'Delivery Tool Platform', 'label')
+    #df = feature_processing.chose_feature_encoding(df, 'Display Course Type', 'label')
+    #df = feature_processing.chose_feature_encoding(df, 'Course Name', 'basen')
+    #df = feature_processing.chose_feature_encoding(df, 'Course Skill', 'basen')
+    #df = feature_processing.chose_feature_encoding(df, 'Specialization', 'basen')
+    #df = feature_processing.chose_feature_encoding(df, 'Training Provider', 'basen')
+    #df = feature_processing.chose_feature_encoding(df, 'Main Domain', 'basen')
+    #df = feature_processing.chose_feature_encoding(df, 'Course Code', 'basen')
+
+    test = df[df['Year'] == 2021]
+    train = df[df['Year'] < 2021]
+
+    test.to_csv('/Users/louise.hubert/PycharmProjects/training_predictions/data/test_data.csv', index=False)
+    train.to_csv('/Users/louise.hubert/PycharmProjects/training_predictions/data/train_data.csv', index=False)
+
+    return df, train, test
 
 def train_with_2021_test(data_train,data_test):
 
@@ -113,5 +143,10 @@ def train_split_function(data):
 
 
 
-train_with_2021_test('/Users/louise.hubert/PycharmProjects/training_predictions/data/train_data.csv','/Users/louise.hubert/PycharmProjects/training_predictions/data/test_data.csv')
-train_split_function('/Users/louise.hubert/PycharmProjects/training_predictions/data/encoded_data.csv')
+DF, TRAIN, TEST = encode('/Users/louise.hubert/PycharmProjects/training_predictions/data/processed_data.csv')
+
+
+train_split_function(DF)
+train_with_2021_test(TRAIN,TEST)
+#train_with_2021_test('/Users/louise.hubert/PycharmProjects/training_predictions/data/train_data.csv','/Users/louise.hubert/PycharmProjects/training_predictions/data/test_data.csv')
+#train_split_function('/Users/louise.hubert/PycharmProjects/training_predictions/data/encoded_data.csv')
